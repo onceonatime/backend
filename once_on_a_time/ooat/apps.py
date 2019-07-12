20fbfb42c56b4e57d901344ef536ac8c8854d793
giyeon0312 <giyeon0312@gmail.com>
2019-07-12 13:51:52 +0900

    first commit

---
+++ once_on_a_time/ooat/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class OoatConfig(AppConfig):
+    name = 'ooat'
---
+++ once_on_a_time/ooat/views.py
@@ -0,0 +1,33 @@
+from .models import Datas
+from django.shortcuts import render
+from django.views.generic import ListView
+#rest_framework의 클래스 기반 뷰인 APIView
+from rest_framework.views import APIView
+#
+from rest_framework.response import Response
+
+# serializer를 가져오기
+from .serializers import DatasSerializer
+
+
+
+
+
+
+
+
+class DatasLV(ListView):
+    model = Datas
+    context_object_name= 'datas'
+    template_name='home.html'
+    
+class DatasList(APIView):
+    #List를 읽기 위한 메서드
+    def get(self,request,format=None):
+        datas=Datas.objects.all()
+        serializer = DatasSerializer(datas,many=True)
+        return Response(serializer.data)
+
+# def home(request):
+#     return render(request,'home.html')
+
